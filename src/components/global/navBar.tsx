b8f9955354af177cb6b2528bf7b7f35377a3376c
Rashmi Ranjan Parija <rashmipersonal@Rashmis-MacBook-Air.local>
2024-07-17 03:47:34 +0530

    Added Clerk Auth and setting up the SignIn and SignOut

--- src/components/global/navBar.tsx
+++ src/components/global/navBar.tsx
@@ -1,21 +1,39 @@
 import {
-    SignInButton,
-    SignedIn,
-    SignedOut,
-    UserButton
-  } from '@clerk/nextjs'
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import { ModeToggle } from "@/components/global//mode-toggle";
+import { Separator } from "../ui/separator";
+import { ChevronRight } from "lucide-react";
 export const NavBar = () => {
-    return (
-        <div>
-            <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-            <ModeToggle/>
-        </div>
-    )
-}
+  return (
+    <>
+      <div className="flex justify-end p-5 ">
+        <div className="flex gap-4 mr-4">
+          <SignedOut>
+          <button className="px-4 py-2 rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200">
+  <SignUpButton/>
+</button>
+            <button className=" flex items-center px-4 py-2 rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200">
+  <SignInButton/>
+  <ChevronRight />
 
+</button>
+          </SignedOut>
+        </div>
+        <div className="flex gap-4 mr-4">
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+        <div>
+          <ModeToggle />
+        </div>
+      </div>
+      <Separator />
+    </>
+  );
+};
